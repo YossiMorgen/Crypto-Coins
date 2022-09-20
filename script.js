922bb0b5e64e852b22459934f1755f6154393543
yossi <a534158711@gmail.com>
2022-09-20 14:52:55 +0300

    added ignore

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-$("button").click( function(){
-    if($(this).next().text() === ""){
-        $(this).next().text(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolorum error autem accusantiu`).hide().slideDown(300)
-    }else{
-        $(this).next().slideUp().text("")
-    }
-})
+$(
+    $("button").click( function(){
+        if($(this).next().text() === ""){
+            $(this).next().text(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolorum error autem accusantiu`).hide().slideDown(300)
+        }else{
+            $(this).next().slideUp().text("")
+        }
+    })
+)
+
+
 
 
