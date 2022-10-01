4d55741aae99107659f752224747c26d8ef0ae35
yossi <a534158711@gmail.com>
2022-10-01 23:35:59 +0300

    add basic about

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let fiveCoins = [];
 let coins = [];
 hideAside();
 window.addEventListener('popstate', e => moveTo(e.state.target))
+let container = document.getElementById("container");
 
 function navTo(el) {
     event.preventDefault();
@@ -44,16 +45,17 @@ function nav() {
 }
 
 function about() {
-    return `<div class="block about">
-                <p>This is the about page</p>
-            </div>`;
-
+    document.getElementById("container").innerHTML  = `<section id="about">
+                <h2>this is a crypto coin project for a college assignment </h2>
+                <div>my name is yossi and i built this project as js ajax jquery project</div>
+                <div>this project first show list of 100 coins toggle button and info button</div>
+                <div>with the toggle button you'll be able to choose five coins to get even more information</div>
+                <img src="assets/climing.png" alt="">
+            </section>` 
 }
 
 function liveReport() {
-    return `<div class="block liveReport">
-                <p>This is the live report page</p>
-            </div>`;
+    // <link rel="icon" type="image/png" href="http://example.com/image.png" />
 }
 
 async function home() {
@@ -64,25 +66,31 @@ async function home() {
     search.type = "search"
     let btn = document.createElement("button")
     btn.innerText = "search";
+    search.addEventListener("change", ()=>{
+        console.log($("input").val());
+        if($("input").val() == ""){
+            createPlacecHolder();
+            creatCoins(coins)
+        }
+    })
     btn.addEventListener("click", ()=>{
-            console.log("hi");
         let inp = $("input").val();
         if(inp.length < 2) {
-            console.log("Too Short");
+            alert("Too Short");
             return;
         }    
-        console.log(inp);
-        console.log(coins);
+        createPlacecHolder();
         let searchCoins = coins.filter(coin => coin.name.search(inp) != -1);
-        console.log(searchCoins);
         creatCoins(searchCoins)
     })
     div.appendChild(search)
     div.appendChild(btn)
+    
     // $("#mainHead").append(`<div id="search">
     //         <input type="search">
     //         <button onclick="search()">search</button>
     //     </div>`)
+
     const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
     const data = await response.json();
     coins = data.slice(0, 100)
@@ -91,7 +99,6 @@ async function home() {
 }
 
 function creatCoins(currentCoins){
-    let container = document.getElementById("container");
     let html = ``
     currentCoins.forEach(coin => {
         html += `
@@ -112,7 +119,6 @@ function creatCoins(currentCoins){
 
 // add or dalete from the coins list
 function chartList(slider){
-    // event.preventDefault();
     const id = $(slider).children("input").attr('id');
     if((fiveCoins.findIndex(coin => coin === id)) === -1){
         if(fiveCoins.length >= 5){
@@ -149,7 +155,7 @@ function DeleteCoinJumpingWindow(btn){
 // find the faster and trutworthy way to get information about the coin
 function findInfo(btn) {
     if(!$(btn).next().length){
-        $(btn).parent().append("<div>").children("button + div").addClass(`${btn.id}div`).html($('<img>',{class:'smlPlaceHolder',src:'bitcoin.png'})).hide().slideDown(300)
+        $(btn).parent().append("<div>").children("button + div").addClass(`${btn.id}div`).html($('<img>',{class:'smlPlaceHolder',src:'assets/bitcoin.png'})).hide().slideDown(300)
     }else{
         $(btn).next().remove(); return;
     }
@@ -172,6 +178,8 @@ function findInfo(btn) {
 
 // edit coins's information list
 function addInfo(info){
+    console.log(info);
+
     let infoArray = getInfo(); 
     const information = {
         "time": new Date().toString().slice(0, 21),
@@ -189,6 +197,10 @@ function addInfo(info){
 
 // show the coin's information on the collapse div
 function showInfo(info){
+    if(info.eur === undefined){
+        $(`.${info.id}div`).html(`sorry but we don't have any information about this coin`).css("background-image", `url("${info.image}")`).css("color", `red`)
+        return;
+    }
     $(`.${info.id}div`).html(`&euro;${info.eur} &nbsp; &nbsp; &#8362;${info.ils} &nbsp; &nbsp;  &#x24;${info.usd}`).css("background-image", `url("${info.image}")`)
 }
 
@@ -210,9 +222,11 @@ function hideAside(){
 // creat coin place holder
 function createPlacecHolder(){
     var img = document.createElement('img');
-    img.src = "bitcoin.png"
+    img.src = "assets/bitcoin.png"
     img.classList.add("placeHolder")
-    document.getElementById('container').appendChild(img);
+    let container = document.getElementById("container");
+    container.innerHTML = ""
+    container.appendChild(img);
 }
 
 
