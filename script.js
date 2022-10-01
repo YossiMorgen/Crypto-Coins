490821e604a3ec785615442efd524369231f53e0
yossi <a534158711@gmail.com>
2022-10-01 17:59:44 +0300

    nav is ready

--- script.js
+++ script.js
@@ -1,21 +1,100 @@
+const url = new URL(window.location);
+moveTo(url.hash);
 let fiveCoins = [];
-hideAside();
 let coins = [];
-$.ajax({
-    url: "https://api.coingecko.com/api/v3/coins/list",
-    success: data => {
-        coins = data.slice(0, 100);
-        creatCoins(coins);
-    },
-    error: err => console.log(err)
-});
+hideAside();
+window.addEventListener('popstate', e => moveTo(e.state.target))
 
-function creatCoins(data){
-    $("#container").html("")
-    let html = "";
-    data.forEach(coin => {
-        // console.log(coin.name.replace(/./g,'').replace(/ /g,''));
-        html += (`
+function navTo(el) {
+    event.preventDefault();
+    // createPlacecHolder()
+    const target = el.dataset.target;
+    moveTo(target);
+    document.querySelectorAll('nav > a').forEach(e => e.classList.remove('active'));
+    document.querySelector(`[data-target='${target}']`).classList.add('active');
+    history.pushState({ target: target }, 'target', target);
+}
+
+
+
+
+function moveTo(target) {
+    createPlacecHolder();
+    app.innerHTML = nav();
+    let html;
+    switch (target) {
+        case '#home': home(); break;
+        case '#about': html =  about(); break;
+        case '#liveReport': html = liveReport(); break;
+        default: html = home(); break;
+    }
+    // document.getElementById("container").innerHTML = html
+}
+
+function nav() {
+    return `
+        <section id="mainHead">
+            <nav>
+                <a onclick="navTo(this)" class="${url.hash == '#home' && 'active'}" data-target='#home' href="">Home</a>
+                <a onclick="navTo(this)" class="${url.hash == '#about' && 'active'}" data-target='#about' href="#about">About</a>
+                <a onclick="navTo(this)" class="${url.hash == '#liveReport' && 'active'}" data-target='#liveReport' href="#liveReport">Live Report</a>
+            </nav>
+        </section>
+        `;
+}
+
+function about() {
+    return `<div class="block about">
+                <p>This is the about page</p>
+            </div>`;
+
+}
+
+function liveReport() {
+    return `<div class="block liveReport">
+                <p>This is the live report page</p>
+            </div>`;
+}
+
+async function home() {
+    let div = document.createElement("div")
+    div.id = "search"
+    document.getElementById("mainHead").appendChild(div)
+    let search = document.createElement("input")
+    search.type = "search"
+    let btn = document.createElement("button")
+    btn.innerText = "search";
+    btn.addEventListener("click", ()=>{
+            console.log("hi");
+        let inp = $("input").val();
+        if(inp.length < 2) {
+            console.log("Too Short");
+            return;
+        }    
+        console.log(inp);
+        console.log(coins);
+        let searchCoins = coins.filter(coin => coin.name.search(inp) != -1);
+        console.log(searchCoins);
+        creatCoins(searchCoins)
+    })
+    div.appendChild(search)
+    div.appendChild(btn)
+    // $("#mainHead").append(`<div id="search">
+    //         <input type="search">
+    //         <button onclick="search()">search</button>
+    //     </div>`)
+    const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
+    const data = await response.json();
+    coins = data.slice(0, 100)
+
+    creatCoins(coins)    
+}
+
+function creatCoins(currentCoins){
+    let container = document.getElementById("container");
+    let html = ``
+    currentCoins.forEach(coin => {
+        html += `
         <div class="coin">
             <span>${coin.name}</span>
             <label onchange="chartList(this)" class="switch">
@@ -26,13 +105,14 @@ function creatCoins(data){
             <div>${coin.symbol}</div>
 
             <button id="${coin.id}" onclick="findInfo(this)" class="btn btn-primary" type="button">more info</button>
-        </div>`)
+        </div>`
     });
-    $("#container").html(html);
+    container.innerHTML = html;
 }
 
-
+// add or dalete from the coins list
 function chartList(slider){
+    // event.preventDefault();
     const id = $(slider).children("input").attr('id');
     if((fiveCoins.findIndex(coin => coin === id)) === -1){
         if(fiveCoins.length >= 5){
@@ -46,35 +126,25 @@ function chartList(slider){
     console.log(fiveCoins);
 }
 
+// showing jumping window and create the coins
 function showJumpingWindow(){
     let html = ""
     fiveCoins.forEach(coin => {        
-        html += `<div><span>${coin}</span><button onclick="DeleteFromFiveCoins(this)">remove</button></div>`
+        html += `<div><span>${coin}</span><button onclick="DeleteCoinJumpingWindow(this)">remove</button></div>`
     })
     $("aside").show(500).children("section").html(html)
 }
-function DeleteFromFiveCoins(btn){
+
+// delete the coin the user chose on the jumping window from the list
+function DeleteCoinJumpingWindow(btn){
     let id = $(btn).siblings("span").text();
     fiveCoins = fiveCoins.filter(coin => coin !== id);
     hideAside();
     document.getElementById(id).checked = false;
-    // $(`#${id}`).children("input").prop('checked', false);
-    console.log(fiveCoins);
 }
+
 // (()=>{
 // })()
-
-// function search(){
-//     let inp = $("input").val();
-//     if(inp.length < 2) {
-//         console.log("Too Short");
-//         return;
-//     }    
-//     let searchData = coins.filter(coin => )
-//     console.log(searchData);
-//     ShowResults(searchData)
-// }
-
 
 // find the faster and trutworthy way to get information about the coin
 function findInfo(btn) {
@@ -94,7 +164,10 @@ function findInfo(btn) {
         }
     }
     saveInfo(info)
-    get_data(`https://api.coingecko.com/api/v3/coins/${btn.id}`, addInfo);
+    // get_data(`btn.id`, addInfo);
+    fetch(`https://api.coingecko.com/api/v3/coins/${btn.id}`)
+        .then((response) => response.json())
+        .then((data) => addInfo(data));
 }
 
 // edit coins's information list
@@ -105,23 +178,14 @@ function addInfo(info){
         "minuts": new Date().getMinutes(),
         "id": info.id,
         "image": info.image.small, 
-        "usd": info.market_data.current_price.usd, 
-        "eur": info.market_data.current_price.eur, 
+        "usd": info.market_data.current_price.usd,
+        "eur": info.market_data.current_price.eur,
         "ils": info.market_data.current_price.ils
     }
     infoArray.push(information);
     saveInfo(infoArray);
     showInfo(information)
 } 
-
-// get api data with call back
-function get_data(api, call_back){
-    $.ajax({
-        url: api,
-        success: data => call_back(data),
-        error: err => console.log(err)
-    })
-}
 
 // show the coin's information on the collapse div
 function showInfo(info){
@@ -143,7 +207,13 @@ function hideAside(){
     $("aside").hide();
 }
 
-
+// creat coin place holder
+function createPlacecHolder(){
+    var img = document.createElement('img');
+    img.src = "bitcoin.png"
+    img.classList.add("placeHolder")
+    document.getElementById('container').appendChild(img);
+}
 
 
 
