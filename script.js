e9d6bbe0ae99bd57123a1b129c351459c5d713de
yossi <a534158711@gmail.com>
2022-10-17 15:15:11 +0300

    LiveReport

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ function navTo(el) {
 }
 
 function moveTo(target) {
+    hide("#chartContainer")
+
     document.querySelector("#container").style.display = "none";
+    // document.querySelector("#chartContainer").style.display = "none";
     document.querySelector("#about").innerHTML = " ";
     app.innerHTML = nav();
     let html;
@@ -25,6 +28,7 @@ function moveTo(target) {
         default: html = home(); break;
     }
 }
+document.querySelector("aside > button").addEventListener("click", ()=> hide("aside"))
 
 function nav() {
     return `
@@ -39,24 +43,119 @@ function nav() {
 }
 
 function about() {
-    hide();   
+    hide(".placeHolder");
     document.querySelector("#container").style.display = "none";
     document.getElementById("about").innerHTML  = `<section id="about">
                 <h2>this is a crypto coin project for a college assignment </h2>
                 <div>my name is yossi and i built this project as js ajax jquery project</div>
-                <div>this project first show list of 100 coins toggle button and info button</div>
+                <div>this project first show ".placeHolder"list of 100 coins toggle button and info button</div>
                 <div>with the toggle button you'll be able to choose five coins to get even more information</div>
                 <img src="assets/climing.png" alt="">
             </section>` 
 }
 
-function liveReport() {
+async function liveReport() {
     document.querySelector("#about").innerHTML = " ";
     document.querySelector("#container").style.display = "none";
-    show();
-    // document.getElementById("container").innerHTML = ` <div id="chartContainer"></div>`
+    show(".placeHolder");
+    let fiveCoins = getInfo("fiveCoins");
+    let infoArr = new Array(fiveCoins.length)
+    show("#chartContainer")
+    for(let i = 0; i < fiveCoins.length; i++){
+        try {
+            let result = await getData(`https://min-api.cryptocompare.com/data/price?fsym=${fiveCoins[i][1]}&tsyms=USD&api_key=361e0baeba01d65a81b0c8405542b62e1392156c2c0a565b05fe50d9c8e016ea`);
+            infoArr[i] = [{
+                x: new Date().getTime(),
+                y: result.USD,
+            }]
+            callChar()        
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    console.log(infoArr);
+    let j = 0;
+    let interval = setInterval(async() => {
+        
+        for(let i = 0; i < fiveCoins.length; i++){
+            try {
+                let result = await getData(`https://min-api.cryptocompare.com/data/price?fsym=${fiveCoins[i][1]}&tsyms=USD&api_key=361e0baeba01d65a81b0c8405542b62e1392156c2c0a565b05fe50d9c8e016ea`);
+                infoArr[i].push({
+                    x: new Date().getTime(),
+                    y: result.USD,
+                })
+            } catch (error) {
+                console.log(error);
+            }
+            
+        }
+        j ++;
+        if(j >= 10){
+            clearInterval(interval);
+        }
+        callChar()
+
+    }, 2000);
     
-    // https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key=INSERT-
+    function callChar(){
+        let data = []
+        for (let index = 0; index < fiveCoins.length; index++) {
+            if(infoArr[index][0].y){
+                let arr = {
+                type: "spline",
+                name: fiveCoins[index][1],
+                showInLegend: true,
+                xValueType: "dateTime",
+                xValueFormatString: "DD MMM hh:mm TT",
+                dataPoints:  infoArr[index],
+                }
+                data.push(arr);
+            }
+        }
+        
+        let options = {
+            exportEnabled: true,
+            animationEnabled: true,
+            title:{
+                text: "Coins Live Report"
+            },
+            subtitles: [{
+                text: "Click Legend to Hide or Show Coin Data"
+            }],
+            axisX: {
+                title: "States",
+                labelFormatter: function ( e ) {
+                    return "x: " + e.value;
+                },
+            },
+            axisY: {
+                title: "Coins's Price",
+                titleFontColor: "#4F81BC",
+                lineColor: "#4F81BC",
+                labelFontColor: "#4F81BC",
+                tickColor: "#4F81BC"
+            },
+            toolTip: {
+                shared: true
+            },
+            legend: {
+                cursor: "pointer",
+                itemclick: toggleDataSeries,
+            },
+            data:  data,
+        };
+        console.log(options);
+        $("#chartContainer").CanvasJSChart(options);
+        hide(".placeHolder")
+        function toggleDataSeries(e) {
+            if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                e.dataSeries.visible = false;
+            } else {
+                e.dataSeries.visible = true;
+            }
+            e.chart.render();
+        }
+    }          
 }
 
 async function home() {
@@ -81,7 +180,7 @@ async function home() {
             alert("Too Short");
             return;
         }
-        show();
+        show(".placeHolder");
         let searchCoins = coins.filter(coin => coin.name.search(inp) != -1);
         document.querySelector("#container").innerHTML = " ";
         creatCoins(searchCoins)
@@ -92,17 +191,18 @@ async function home() {
     document.querySelector("#container").style.display = "flex";
     let coins = [];
     console.log(coins);
-    if(document.querySelector("#container").innerHTML !== " "){
-        hide();
+    if(document.querySelector("#container").innerHTML !== " "){ 
+        hide(".placeHolder");
         return;
     }
-    show();
+    show(".placeHolder");
     
     console.log(coins);
     if(coins.length === 0){
         try {
-            coins = await getData('https://api.coingecko.com/api/v3/coins/list')        
-            creatCoins(coins); return;
+            coins = await getData('https://api.coingecko.com/api/v3/coins/list');
+            creatCoins(coins); 
+            return;
         } catch (error) {
             console.log(error);
         }
@@ -113,7 +213,7 @@ async function home() {
 function creatCoins(data){
     let currentCoins = data.slice(0, 100)
     console.log(currentCoins);
-    hide()
+    hide(".placeHolder");
     currentCoins.forEach(coin => {
         let div = document.createElement("div");
         div.classList.add("coin");
@@ -142,20 +242,17 @@ function creatCoins(data){
         div.appendChild(div2);
 
         let btn = document.createElement("button");
-        // btn.type = "button"
         btn.className = "btn-primary";
         btn.classList.add("btn");
         btn.innerText = "More Info";
         let div3 = document.createElement("div");
         div3.className = "hide";
 
-        // 
         btn.addEventListener("click", async()=> {
-            // div3.classList.toggle("hide");
             if(div3.className !== "hide"){
                 div3.classList.add("hide");return;
             }
-            div3.classList.remove("hide")
+            div3.classList.remove("hide");
             if(div3.innerHTML == ""){
                 let placeHolder = document.createElement("img");
                 placeHolder.className = "smlPlaceHolder";
@@ -176,9 +273,6 @@ function creatCoins(data){
                 addInfo(div3, coin.id);
             }
         })
-        // 
-
-
         div.appendChild(btn);
         div.appendChild(div3);
     })
@@ -186,9 +280,9 @@ function creatCoins(data){
     console.log(fiveCoins);
     fiveCoins.forEach(arrCoin => {
         console.log(arrCoin);
-        for (const checkCoin of document.getElementsByClassName(arrCoin)) {
+        for (const checkCoin of document.getElementsByClassName(arrCoin[0])) {
             console.log(checkCoin);
-            if (checkCoin.className === arrCoin) 
+            if (checkCoin.className === arrCoin[0]) 
                 checkCoin.checked = "true";
             }
         console.log(arrCoin);
@@ -203,6 +297,7 @@ async function addInfo(div, coin){
     } catch (error) {
         console.log(error);
     }
+    console.log(info);
     let infoArray = getInfo(`coinsInfo`);
     const information = {
         "time": new Date().toString().slice(0, 21),
@@ -219,90 +314,91 @@ async function addInfo(div, coin){
 }
 
 // show the coin's information on the collapse div
-function showInfo(info, div){   
+function showInfo(info, div){
+    if(typeof (info.eur) === "undefined"){
+        console.log("undefined");
+        div.innerHTML = `sorry we can't find the info you wanted`
+        div.style.color = "red"
+        return;
+    }
     div.innerHTML = `&euro;${info.eur} &nbsp; &nbsp; &#8362;${info.ils} &nbsp; &nbsp;  &#x24;${info.usd}`
     let img = document.createElement("img")
     img.setAttribute("src", info.image)
     div.appendChild(img);
 }
 
+// manage the five coins's list
 function fiveCoinsList(input, coin){
     let fiveCoins = getInfo("fiveCoins");
     console.log(fiveCoins);
     if(document.querySelector("aside").className !== "hide"){
         input.checked = !(input.checked); return;
     }
-    if(input.checked == true && fiveCoins.find(arrCoin => arrCoin === coin.name) !== -1){
+    if(input.checked == true && fiveCoins.find(arrCoin => arrCoin[0] === coin.name) !== -1){
         if(fiveCoins.length > 4){
             input.checked = false;
-            showJumpingWindow(fiveCoins, input); 
-            console.log(fiveCoins);
+            showJumpingWindow(fiveCoins, input, coin.symbol); 
             return;
         }
-        fiveCoins.push(coin.name);
-        saveInfo(fiveCoins, "fiveCoins") 
-        console.log(fiveCoins);
+        fiveCoins.push([coin.name, coin.symbol]);
+        saveInfo(fiveCoins, "fiveCoins");
         return;
     }
-    console.log(coin.name);
-    fiveCoins = fiveCoins.filter(arrCoin => arrCoin !== coin.name); 
+    fiveCoins = fiveCoins.filter(arrCoin => arrCoin[0] !== coin.name); 
     saveInfo(fiveCoins, "fiveCoins");
-    console.log(fiveCoins);
 }
 
 // showing jumping window and create the coins
-function showJumpingWindow(fiveCoins, nextBtn){
+function showJumpingWindow(fiveCoins, nextBtn, symbol){
     let section = document.querySelector("aside > section");
     section.innerHTML = " ";
     fiveCoins.forEach(coin => {
         let div = document.createElement("div");
         section.appendChild(div);
         let span = document.createElement("span");
-        span.innerHTML = coin;
+        span.innerHTML = coin[0];
         div.appendChild(span);
         let btn = document.createElement("button");
         btn.innerText = "remove";
         btn.addEventListener("click", ()=>{
             nextBtn.checked = "true";
-            fiveCoins.push(nextBtn.className);
-            fiveCoins = fiveCoins.filter(arrCoin => arrCoin !== coin);
+            fiveCoins.push([nextBtn.className, symbol]);
+            fiveCoins = fiveCoins.filter(arrCoin => arrCoin[0] !== coin[0]);
             saveInfo(fiveCoins, "fiveCoins")
             console.log(fiveCoins);
-            hideAside();
-            document.getElementsByClassName(coin)[0].checked = false;
+            hide("Aside");
+            let oldSlider = document.getElementsByClassName(coin[0])[0];
+            if(oldSlider){
+                oldSlider.checked = false;
+            }
         })
         div.appendChild(btn);
     })
-    document.querySelector("aside").classList.remove("hide");
+    show("aside")
 }
 
-// save the update coins's information
+// save the update local storage info
 function saveInfo(info, name){
     localStorage.setItem(name, JSON.stringify(info));
 }
 
-// get the available information about the coins 
+// get the available information in local storage
 function getInfo(name){
     return JSON.parse(localStorage.getItem(name) || "[]")
 }
 
-// hide the jumping window
-function hide(){
-    document.querySelector("aside").classList.add("hide")
-}
-
 // show coin place holder
-function show(){
-    document.querySelector(".placeHolder").classList.remove("hide")
+function show(element){
+    document.querySelector(element).classList.remove("hide")
 }
 
-// show coin place holder
-function hide(){
-    document.querySelector(".placeHolder").classList.add("hide")
+// hide coin place holder
+function hide(element){
+    document.querySelector(element).classList.add("hide")
 }
 
+// returning async data for url
 async function getData(url) {
-    console.log(url);
     const response = await fetch(url);
     return await response.json();
 }
